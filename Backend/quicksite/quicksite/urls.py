8df0ab7bc4a077504cb1edbbbb2e06cde807dea8
M Sajid Mansoori <sajid.mansoori12@gmail.com>
2020-11-17 08:26:51 +0530

    Created the user api

--- Backend/quicksite/quicksite/urls.py
+++ Backend/quicksite/quicksite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from User_Quicksite import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users',views.UserView,'user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(router.urls))
 ]
