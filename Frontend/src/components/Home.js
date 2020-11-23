a33b2d92378c5f01e76e6965647b703f05d49c32
vikassoni99 <vikassoni.soni22@gmail.com>
2020-11-23 21:20:31 +0530

    Added Carousel

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Navbar from './Navbar/Navbar'
+import CarouselBanner from './CarouselBanner/CarouselBanner'
 
 const Home = (props) => {
     return(
         <>
         <Navbar/>
+        <CarouselBanner/>
         </>        
     );
 }
